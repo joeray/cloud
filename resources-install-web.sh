4727d03b0df40425af4da251efbc1cb705b3a29d
josé ramón couñago <jramonroza@gmail.com>
2025-04-25 10:24:58 +0000

    Update resources-install-web.sh

--- resources-install-web.sh
+++ resources-install-web.sh
@@ -24,9 +24,9 @@ echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
 gsutil mb gs://$DEVSHELL_PROJECT_ID
 
-gsutil cp gs://sureskills-ql/challenge-labs/ch01-startup-script/install-web.sh gs://$DEVSHELL_PROJECT_ID
+gsutil cp gs://D:\Users\counajo\Downloads/resources-install-web.sh gs://$DEVSHELL_PROJECT_ID
 
-gcloud compute instances create quickgcplab --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=n1-standard-1 --tags=http-server --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID/install-web.sh
+gcloud compute instances create cloud --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=n1-standard-1 --tags=http-server --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID/resources-install-web.sh
 
 gcloud compute firewall-rules create allow-http \
     --allow=tcp:80 \
